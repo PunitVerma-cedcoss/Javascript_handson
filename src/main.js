a72fd05ee3d55871e06e1e5d8c21cd156a1b6ba1
= <=>
2022-02-05 14:06:48 +0530

    Iterate Odd Numbers With a For Loop

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 // Setup
 const myArray = [];
-for(var i=1;i<=5;i++)
-{
-  myArray.push(i)
-}
+
 // Only change code below this line
+for(var i=0;i<=10;i++)
+{
+  if(i%2 != 0)
+    myArray.push(i)
+}
