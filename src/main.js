3836d1a3198dcc014083827743da64afe6fadae7
= <=>
2022-02-05 13:24:33 +0530

    Accessing Nested Objects

--- src/main.js
+++ src/main.js
@@ -1,23 +1,13 @@
-const myMusic = [
-  {
-    "artist": "Billy Joel",
-    "title": "Piano Man",
-    "release_year": 1973,
-    "formats": [
-      "CD",
-      "8T",
-      "LP"
-    ],
-    "gold": true
-  },
-  {
-    "artist": "myself",
-    "title": "heck",
-    "release_year": 2022,
-    "formats": [
-      "CD",
-      "8T",
-      "LP"
-    ]
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+    },
+    "outside": {
+      "trunk": "jack"
+    }
   }
-];
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
