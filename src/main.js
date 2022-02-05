fb990b39685d3f236c4ba49b1e7f097a1d63665b
= <=>
2022-02-05 11:47:55 +0530

    Local Scope and Functions

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ function myLocalScope() {
     var myVar;
     console.log('inside myLocalScope', myVar);
   }
-  myLocalScope();
-  
+  myLocalScope();  
   // Run and check the console
   // myVar is not defined outside of myLocalScope
   console.log('outside myLocalScope', myVar);
