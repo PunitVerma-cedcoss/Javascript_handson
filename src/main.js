4a030761226a5bc46d70b0ba0bc224f8e47c412f
= <=>
2022-02-05 13:13:05 +0530

    Delete Properties from a JavaScript Object

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+// Setup
 const myDog = {
   "name": "Happy Coder",
   "legs": 4,
   "tails": 1,
-  "friends": ["freeCodeCamp Campers"]
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
 };
 
-myDog.bark = "bow-bow"
+// Only change code below this line
+
+delete myDog.tails
