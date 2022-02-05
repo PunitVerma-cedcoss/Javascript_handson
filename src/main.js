7102306d24e11ebc7044af09568ea6efa62563dd
= <=>
2022-02-05 13:04:58 +0530

    Build JavaScript Objects

--- src/main.js
+++ src/main.js
@@ -1,32 +1,8 @@
-let count = 0;
-
-function cc(card) {
+const myDog = {
   // Only change code below this line
-  switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++
-      break
-    case 7:
-    case 8:
-    case 9:
-      break
-    case 10:
-    case 'J':
-    case 'Q':
-    case 'K':
-    case 'A':
-      count--
-      break
-  }
-  if (count > 0) {
-    return `${count} Bet`
-  }
-  else if (count <= 0) {
-    return `${count} Hold`
-  }
-}
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+  name:"bull dog",
+  legs:2,
+  tails:5,
+  friends:["a","4","4"]
+  // Only change code above this line
+};
