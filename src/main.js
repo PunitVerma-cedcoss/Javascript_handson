d69bceb4a631bed5ad0fee234fc8a9a76e31de19
= <=>
2022-02-05 12:49:08 +0530

    Multiple Identical Options in Switch Statements

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-function switchOfStuff(val) {
+function sequentialSizes(val) {
   let answer = "";
   // Only change code below this line
-  switch (val) {
-    case "a":
-      answer = "apple"
-      break
-    case "b":
-      answer = "bird"
-      break
-    case "c":
-      answer = "cat"
-      break
-    default:
-      answer = "stuff"
-      break
-
+  switch(val){
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low"
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid"
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High"
+      break;
   }
 
 
@@ -22,4 +24,4 @@ function switchOfStuff(val) {
   return answer;
 }
 
-switchOfStuff(1); 
+sequentialSizes(1);
