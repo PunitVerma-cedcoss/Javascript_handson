9fd59a31a40d1a6971a5cf0164b5b86a0d66f175
= <=>
2022-02-05 13:02:19 +0530

    Returning Boolean Values from Functions

--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
-// Setup
-function abTest(a, b) {
+let count = 0;
+
+function cc(card) {
   // Only change code below this line
-  if(a < 0 || b < 0)
-    return 
-
-
-  // Only change code above this line
-
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++
+      break
+    case 7:
+    case 8:
+    case 9:
+      break
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--
+      break
+  }
+  if (count > 0) {
+    return `${count} Bet`
+  }
+  else if (count <= 0) {
+    return `${count} Hold`
+  }
 }
 
-abTest(2,2);
+cc(2); cc(3); cc(7); cc('K'); cc('A');
