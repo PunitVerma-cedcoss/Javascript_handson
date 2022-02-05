2500a4170f7edd24b2dc1b1d2546d6f01ece03ef
= <=>
2022-02-05 14:12:16 +0530

    Count Backwards With a For Loop

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@
 const myArray = [];
 
 // Only change code below this line
-for(var i=0;i<=10;i++)
+for(var i=9;i>0;i--)
 {
   if(i%2 != 0)
-    myArray.push(i)
+    {
+      myArray.push(i)
+    }
 }
+console.log(myArray)
