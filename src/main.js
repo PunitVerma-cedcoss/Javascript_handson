da313400e58d058e7488ad7eb485d40a49bccfe2
= <=>
2022-02-05 11:11:38 +0530

    Appending Variables to Strings

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
-// Only change code below this line
-const myName = "Punit";
-const myStr = "My name is " + myName + "and I am well!";
+// Change code below this line
+const someAdjective = "three";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
