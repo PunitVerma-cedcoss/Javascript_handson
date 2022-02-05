ed293c4d7bc6e04632f1d78358a1667bc59e04c5
= <=>
2022-02-05 11:49:05 +0530

    Global vs. Local Scope in Functions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-function myLocalScope() {
-    // Only change code below this line
-    var myVar;
-    console.log('inside myLocalScope', myVar);
-  }
-  myLocalScope();  
-  // Run and check the console
-  // myVar is not defined outside of myLocalScope
-  console.log('outside myLocalScope', myVar);
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  var outerWear = "sweater";
+  // Only change code below this line
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
